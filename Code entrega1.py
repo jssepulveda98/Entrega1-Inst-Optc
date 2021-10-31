cbe4e59cc6bf5e7020c9c222c9a9e0d9d93e152d
jssepulveda98 <93456441+jssepulveda98@users.noreply.github.com>
2021-10-31 15:25:23 -0500

    Update Code entrega1.py

--- Code entrega1.py
+++ Code entrega1.py
@@ -1,12 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy as sp
-
+from scipy.ndimage import gaussian_filter
+import cv2
 
 #Dimensiones y constantes
+ABC=np.loadtxt('a.txt',delimiter=',', dtype=complex)
 
+A=[]
+
+print(ABC[0:1])
+
+ABC=np.fft.ifft(ABC)
 
 
 #Funciones
+
+def f4G(Imagen):
+    Imagenf=np.fft.fft2(Imagen)
+    filtro=gaussian_filter(Imagen,2.8)
+    Imagenff=np.fft.ifft2(Imagenf)
+    
+    
+    return filtro
+
+
+
+    
+imagen= cv2.imread("b.png")
+
+#plt.imshow(ABC)
+#plt.imshow(f4G(imagen))
+
+
+plt.show()
+
+
+
+
+
+
 
 
