a4528bf5cc7c3a7dd7ab82e1ef41215cc4619080
alegomezram <alegomezram@unal.edu.co>
2021-11-03 01:03:46 -0500

    Punto 3 pero sin atribuir condiciones reales a la discretización

---
+++ IO_Entrega1_Punto3_sin_discretizar.py
@@ -0,0 +1,78 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+
+#Functions
+
+def firstlens(t1, w_length, f_length):
+	"""
+	Fourier transform done by the first lens
+	Incident optical field: plane and monochromatic wave
+	Lens bigger than transmittance: pupil function equals 1
+	"""
+
+	Uf1=(1/(1j*w_length*f_length))*np.fft.fft2(t1)
+	Uf1=np.fft.fftshift(Uf1)
+
+	return Uf1
+
+def secondlens(Uf1, w_length, f_length):
+	"""
+	Fourier transform done by the first lens
+	Incident optical field: plane and monochromatic wave
+	Lens bigger than transmittance: pupil function equals 1
+	"""
+
+	Uf2=(1/(1j*w_length*f_length))*np.fft.fft2(Uf1)
+	Uf2=np.fft.fftshift(Uf2)
+
+	return Uf2
+
+
+
+#Dimensions and constants
+"""
+t1=transmittance
+f_length=focal length
+w_length= wavelength
+deltau=pixel size Fourier plane
+deltav=pixel size Fourier plane
+deltaxprim=pixel size transmittance plane
+deltayprim=pixel size transmittance plane
+M*2=number of pixels in the x' axis
+N*2=number of pixels in the y' axis
+M*2xN*2=number of pixels entrance plane
+"""
+
+M=256 #Number of pixels=M*2 (along one axis number of pixels=512)
+N=256 #Number of pixels=N*2
+f_length=5000  #(50mm)
+w_length=0.633   #All units in um
+
+
+t1= cv2.imread("cameraman.png")
+UF1=firstlens(t1, w_length, f_length)
+
+UF2=secondlens(UF1, w_length, f_length)
+
+I1=(np.abs(UF1)**2)                            #Intensity
+angle1=np.angle(UF1)                           #Phase
+
+I2=(np.abs(UF2)**2)                            #Intensity
+angle2=np.angle(UF2)                           #Phase
+
+
+plt.figure(1) 
+plt.imshow(I1,cmap='gray')
+plt.title('Fourier plane')
+plt.ylabel('[um]')
+plt.xlabel('[um]')
+plt.savefig("Fourier plane2.png",dpi=500)
+
+plt.figure(2) 
+plt.imshow(I2,cmap='gray')
+plt.title('Image')
+plt.ylabel('[um]')
+plt.xlabel('[um]')
+plt.savefig("Image2.png",dpi=500)
